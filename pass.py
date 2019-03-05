803a925e1f2febea7537c2ca90114af56203b903
David <drobinson12@student.waubonsee.edu>
2019-03-05 11:52:22 -0600

    This is a meaningful message for the second commmit of pass.py

--- pass.py
+++ pass.py
@@ -1,15 +1,20 @@
-Python 3.6.7 (default, Oct 22 2018, 11:32:17) 
-[GCC 8.2.0] on linux
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
- RESTART: /home/leach/Desktop/Classes/Spring 2019/Development Tools and Structured Program Design/Lab/Password/Pswrd.py 
-Please enter your password43
-INNCORRECT
->>> 
- RESTART: /home/leach/Desktop/Classes/Spring 2019/Development Tools and Structured Program Design/Lab/Password/Pswrd.py 
-Please enter your password12345
-INNCORRECT
->>> 
- RESTART: /home/leach/Desktop/Classes/Spring 2019/Development Tools and Structured Program Design/Lab/Password/Pswrd.py 
-Please enter your password123456
->>> 
+## Password
+
+
+
+psswrd = ('123456')
+
+pword = input(str("Please enter your password"))
+
+pGood = False
+
+while(pGood == False):
+
+    pword = input(str("Please enter your password"))
+    if (psswrd != pword):
+
+        print ("INNCORRECT")
+    else:
+        print ("CORRECT!")
+        pGood = True
+## Test
